e71d7cff6064ed8ceb57e6b49900cf3b075ea401
iskolbin <iskolbin@gmail.com>
2017-10-11 00:31:06 +0300

    major rewrite

---
+++ README.md
@@ -0,0 +1,48 @@
+Quad Tree
+=========
+
+Persistent quad tree implementation for Lua. It's more like persistent spatial
+hash structure actually. All operations are non-mutating.
+
+QuadTree.make( width, height, levels = 3 )
+--------------------------------------------
+
+Create new quad tree structure. To optimize spatial queries `levels` variable
+defines the number of spatial subdivisions, for example `levels = 3` (default)
+divide domain in 64 rectangles.
+
+Optimal value of `levels` highly depends on the task, especially on the average
+size of spatial objects. Good approximation is size of the smallest subdivision
+is x2 of average size of object, for example for 32x32 px object for 1920x1080
+field 5 levels will be ok, size of leaf will be 60x33,75.
+
+As you already guessed it will perform badly if you have lots of highly
+different size objects. While internally this is a quadtree, it performs like
+spatial hashing schemes.
+
+QuadTree.insert( qt, x, y, width, height )
+------------------------------------------
+
+Inserts new rectangle in the tree and updates internal `id` counter. Returns
+updated tree and table (`rid`:  x0, y0, x1, y1 and id 5-element table) which
+can be used for `remove` and `update`.
+
+QuadTree.remove( qt, rid )
+--------------------------
+
+Removes entity specified by `rid` with  from the tree. Returns updated tree.
+
+QuadTree.update( qt, rid, nx = rid[1], ny = rid[2], nw = rid[3]-rid[1], nh = rid[4]-rid[2] )
+--------------------------------------------------------------------------------------------
+
+Updates entity with `rid` in the tree. Essentially this is `remove` followed by
+`insert` without new id generation. In the future this method must be optimized
+by not touching overlaping part. Returns updated tree and updated table for
+`remove`/`update`.
+
+QuadTree.get( qt, x, y, width = 0, height = 0 )
+-----------------------------------------------
+
+Gets all entities stored in leaf rectangles overlapping with specified rectangle
+(or point). Returns table filled with id as key-value pairs (not array). After
+that you have to filter properly.
--- quadtree.lua
+++ quadtree.lua
@@ -20,7 +20,6 @@ function QuadTree.make( width, height, levels )
 	levels = levels or 3
 	return {
 		id = 'QuadTree',
-		cellsize = cellsize,
 		root = makeNodes( levels ),
 		id = 0,
 		width = width,
@@ -29,7 +28,7 @@ function QuadTree.make( width, height, levels )
 	}
 end
 
-local function queryGet( node, level, x, y, width, height, xywh, result )
+local function queryGet( node, level, x, y, width, height, x0, y0, x1, y1, result )
 	if level <= 0 then
 		for i = 1, #node do
 			result[node[i]] = node[i]
@@ -37,58 +36,58 @@ local function queryGet( node, level, x, y, width, height, xywh, result )
 		return result
 	else
 		local hw, hh = width / 2, height / 2
-		if xywh[1] <= x + hw and xywh[2] <= y + hh and xywh[3] >= x and xywh[4] >= y then
-			queryGet( node[1], level-1, x, y, hw, hh, xywh, result )
+		if x0 <= x + hw and y0 <= y + hh and x1 >= x and y1 >= y then
+			queryGet( node[1], level-1, x, y, hw, hh, x0, y0, x1, y1, result )
 		end
-		if xywh[1] <= x + width and xywh[2] <= y + hh and xywh[3] >= x + hw and xywh[4] >= y then
-			queryGet( node[2], level-1, x+hw, y, hw, hh, xywh, result )
+		if x0 <= x + width and y0 <= y + hh and x1 >= x + hw and y1 >= y then
+			queryGet( node[2], level-1, x+hw, y, hw, hh, x0, y0, x1, y1, result )
 		end
-		if xywh[1] <= x + hw and xywh[2] <= y + height and xywh[3] >= x and xywh[4] >= y + hh then
-			queryGet( node[3], level-1, x, y+hh, hw, hh, xywh, result )
+		if x0 <= x + hw and y0 <= y + height and x1 >= x and y1 >= y + hh then
+			queryGet( node[3], level-1, x, y+hh, hw, hh, x0, y0, x1, y1, result )
 		end
-		if xywh[1] <= x + width and xywh[2] <= y + height and xywh[3] >= x + hw and xywh[4] >= y + hh then
-			queryGet( node[4], level-1, x+hw, y+hh, hw, hh, xywh, result )
+		if x0 <= x + width and y0 <= y + height and x1 >= x + hw and y1 >= y + hh then
+			queryGet( node[4], level-1, x+hw, y+hh, hw, hh, x0, y0, x1, y1, result )
 		end
 		return result
 	end
 end
 
-local function queryModify( node, level, x, y, width, height, xywhid, f )
+local function queryModify( node, level, x, y, width, height, rectid, f )
 	if level <= 0 then
-		return f( node, xywhid )
+		return f( node, rectid )
 	else
 		local hw, hh = width / 2, height / 2
 		local node_ = {node[1],node[2],node[3],node[4]}
-		if xywhid[1] <= x + hw and xywhid[2] <= y + hh and xywhid[3] >= x and xywhid[4] >= y then
-			node_[1] = queryModify( node[1], level-1, x, y, hw, hh, xywhid, f )
+		if rectid[1] <= x + hw and rectid[2] <= y + hh and rectid[3] >= x and rectid[4] >= y then
+			node_[1] = queryModify( node[1], level-1, x, y, hw, hh, rectid, f )
 		end
-		if xywhid[1] <= x + width and xywhid[2] <= y + hh and xywhid[3] >= x + hw and xywhid[4] >= y then
-			node_[2] = queryModify( node[2], level-1, x+hw, y, hw, hh, xywhid, f )
+		if rectid[1] <= x + width and rectid[2] <= y + hh and rectid[3] >= x + hw and rectid[4] >= y then
+			node_[2] = queryModify( node[2], level-1, x+hw, y, hw, hh, rectid, f )
 		end
-		if xywhid[1] <= x + hw and xywhid[2] <= y + height and xywhid[3] >= x and xywhid[4] >= y + hh then
-			node_[3] = queryModify( node[3], level-1, x, y+hh, hw, hh, xywhid, f )
+		if rectid[1] <= x + hw and rectid[2] <= y + height and rectid[3] >= x and rectid[4] >= y + hh then
+			node_[3] = queryModify( node[3], level-1, x, y+hh, hw, hh, rectid, f )
 		end
-		if xywhid[1] <= x + width and xywhid[2] <= y + height and xywhid[3] >= x + hw and xywhid[4] >= y + hh then
-			node_[4] = queryModify( node[4], level-1, x+hw, y+hh, hw, hh, xywhid, f )
+		if rectid[1] <= x + width and rectid[2] <= y + height and rectid[3] >= x + hw and rectid[4] >= y + hh then
+			node_[4] = queryModify( node[4], level-1, x+hw, y+hh, hw, hh, rectid, f )
 		end
 		return node_
 	end
 end
 
-local function insert( node, xywhid )
+local function insert( node, rectid )
 	local node_, n = {}, #node
 	for i = 1, n do
 		node_[i] = node[i]
 	end
-	node_[n+1] = xywhid[5]
+	node_[n+1] = rectid[5]
 	return node_
 end
 
-local function remove( node, xywhid )
-	local node_, n, id = {}, #node, xywhid[5]
-	for i = 1, #node do
+local function remove( node, rectid )
+	local node_, n, id = {}, #node, rectid[5]
+	for i = 1, n do
 		if node[i] == id then
-			for j = i+1, #node do
+			for j = i+1, n do
 				node_[j-1] = node[j]
 			end
 			return node_
@@ -99,35 +98,35 @@ local function remove( node, xywhid )
 	return node_
 end
 
+-- TODO
+-- optimze: we should not touch overlapping cells, this will reduce GC work on magnitude scale
+local function queryUpdate( node, level, x, y, w, h, rectid, nrectid )
+	return queryModify( queryModify( node, level, x, y, w, h, rectid, remove ), x, y, w, h, nrectid, insert )
+end
+
 function QuadTree.insert( self, x, y, w, h )
 	local o = copy( self )
 	o.id = o.id+1
-	o.root = queryModify( o.root, self.levels, 0, 0, self.width, self.height, {x, y, x+w, x+h, o.id}, insert )
-	return o
+	local rectid = {x, y, x+w, y+h, o.id}
+	o.root = queryModify( o.root, self.levels, 0, 0, self.width, self.height, rectid, insert )
+	return o, rectid
 end
 
-function QuadTree.remove( self, id, x, y, w, h )
+function QuadTree.remove( self, rectid )
 	local o = copy( self )
-	o.root = queryModify( o.root, self.levels, 0, 0, self.width, self.height, {x, y, x+w, y+h, id}, remove )
+	o.root = queryModify( o.root, self.levels, 0, 0, self.width, self.height, rectid, remove )
 	return o
 end
 
-function QuadTree.update( self, id, x, y, w, h, newx, newy, neww, newh )
-	local o = copy( self )
-	newx, newy, neww, newh = newx or x, newy or y, neww or w, newh or h
-	o.root = queryModify( remove( o.root, self.levels, 0, 0, self.width, self.height, {x, y, x+w, y+h, id}, remove ), 0, 0, self.width, self.height, {newx, newy, newx + neww, newy + newh,id}, insert )
-	return o
+function QuadTree.update( self, rectid, nx, ny, nw, nh )
+	nx, ny, nw, nh = nx or rectid[1], ny or rectid[2], nw or (rectid[3]-rectid[1]), nh or (rectid[4]-rectid[2])
+	local o, nrectid = copy( self ), {nx, ny, nx+nw, ny+nh, rectid[5]}
+	o.root = queryUpdate( self, self.levels, 0, 0, self.width, self.height, rectid, nrectid )
+	return o, nrectid
 end
 
-function QuadTree.move( self, id, x, y, w, h, newx, newy )
-	local o = copy( self )
-	newx, newy = newx or x, newy or y
-	o.root = queryModify( remove( o.root, self.levels, 0, 0, self.width, self.height, {x, y, x+w, y+h, id}, remove ), 0, 0, self.width, self.height, {newx, newy, newx + w, newy+h,id}, insert )
-	return o
-end
-
-function QuadTree.get( self, x, y, w, h )
-	return queryGet( self.root, self.levels, 0, 0, self.width, self.height, {x, y, x+(w or 0), y+(h or 0)}, {} )
+function QuadTree.get( self, x, y, w, h, result )
+	return queryGet( self.root, self.levels, 0, 0, self.width, self.height, x, y, x+(w or 0), y+(h or 0), result or {} )
 end
 
 return QuadTree
